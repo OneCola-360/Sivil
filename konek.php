c70fcfd647f60b577820793c5615ddb27a9b8d96
MBoi <kvkadrptr@gmail.com>
2026-08-19 13:42:14 +0700

    Update konek.php

--- konek.php
+++ konek.php
@@ -1,5 +1,5 @@
 <?php
-$konek = mysqli_connect("127.0.0.1", "", "", "db_sakuci");
+$konek = mysqli_connect("127.0.0.1", "root", "", "db_sakuci");
 
 if (!$konek) die("Koneksi Gagal: " . mysqli_connect_error());
 else echo "Koneksi Berhasil";
